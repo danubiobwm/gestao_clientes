c256072522dd0be3e5a4f3b2b3252a590d79ae21
danubiobwm <danubio.bwm@gmail.com>
2019-06-16 19:31:07 -0300

    commit 16/06

--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from django.views.generic.base import TemplateView
+
+from django.http import HttpResponse
+from django.views import View
+
 
 
 def home(request):
@@ -9,3 +14,14 @@ def home(request):
 def my_logout(request):
     logout(request)
     return redirect('home')
+
+class HomePageView(TemplateView):
+    template_name = 'home2.html'
+
+class MyView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request,'home3.html')
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('Post')
