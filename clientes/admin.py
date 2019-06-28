557e1fde57b7838bea1949446d367559fa778a1c
danubiobwm <danubio.bwm@gmail.com>
2019-06-27 23:47:19 -0300

    deploy 27/06

--- clientes/admin.py
+++ clientes/admin.py
@@ -2,13 +2,23 @@ from django.contrib import admin
 from .models import Person, Documento, Venda, Produto
 
 class PersonAdmin(admin.ModelAdmin):
-    fields=('doc', 'first_name', 'last_name', 'age', 'salary',
-     'bio', 'photo'  )
+    fieldsets = (
+        ('Dados Pessois', {'fields': ( 'doc', 'first_name', 'last_name')}),
+        ('Dados Complementares', { 'classes':('collapse',), 
+        'fields': ('age', 'salary', 'photo' )
+        })
+    )
+    
+    #fields=('doc', 'first_name', 'last_name', 'age', 'salary', 'bio', 'photo'  )
     list_display = ('doc', 'first_name', 'last_name', 'age', 'salary',
      'bio', 'photo' )
+    
+    list_filter = ('age', 'salary')
 
+class VendaAdmin(admin.ModelAdmin):
+    list_filter = ('pessoa__doc', 'desconto')
 
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Documento)
-admin.site.register(Venda)
+admin.site.register(Venda, VendaAdmin)
 admin.site.register(Produto)
